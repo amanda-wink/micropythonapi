7337bc6a6f3920b31a6042360993d0d00bd8a3eb
amanda <amanda@zipcodes-MacBook-Pro.local>
2020-12-19 16:48:52 -0500

    urls and data loading

---
+++ booksapi/books/management/commands/load_csv.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand, CommandError
+from ...models import Book
+import csv
+
+class Command(BaseCommand):
+    help = 'Loads bestseller books'
+
+    def handle(self, *args, **kwargs):
+        with open('/Users/amanda/Documents/PythonProjects/micropythonapi/bestsellers-with-categories.csv') as f:
+            reader = csv.reader(f)
+            next(reader)
+            for row in reader:
+                _, created = Book.objects.get_or_create(
+                    name = row[0],
+                    author = row[1],
+                    rating = row[2],
+                    reviews = row[3],
+                    price = row[4],
+                    year = row[5],
+                    genre = row[6]
+                )
